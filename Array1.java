29f615e481fe25b277cdcb9a216cd28e8549846a
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-02-12 16:17:21 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,12 @@
+class  Array1
+{
+	public static void main(String[] args) 
+	{
+	int a[]={4,9,-1,6,3};
+		System.out.println(a[2]);
+	int b=a[1];
+	    ++b;
+		a[1]=b;
+		System.out.println(a[1]);
+	}
+}
