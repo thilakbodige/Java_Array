a30d067618975ea7f30ae0f60524b2cf45f497df
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-02-13 16:36:19 +0530

    Add files via upload

---
+++ Array6.java
@@ -0,0 +1,18 @@
+class Array6
+{
+ public static void main(String[]args)
+	{
+	 int a[]={-6,-9,-14,-1,-3};
+ int max=a[0];
+ for(int i=0;i<=4;i++)
+{
+	 if(max<=a[i])
+{
+  if(max<=a[i])
+{
+}
+System.out.println("The max Value is: "+ max);
+}
+}
+}
+}
