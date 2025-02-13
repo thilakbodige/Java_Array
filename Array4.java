6e354d8f0ecfdf78055479640b01fb8c25ac3050
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-02-13 16:33:47 +0530

    Add files via upload

---
+++ Array4.java
@@ -0,0 +1,13 @@
+class Array4 
+{
+	public static void main(String[] args) 
+	{
+		int a[]={10,-2,7,1,-19};
+		int sum=0;
+		for(int num:a)
+		{
+			sum=sum+num;
+		}
+		System.out.println("The Sum of the elements is "+sum);
+	}
+}
