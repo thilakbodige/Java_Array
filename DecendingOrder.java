53727cd696081fd1c6e9339267e7d6794ca5880e
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-02-17 16:23:27 +0530

    Add files via upload

---
+++ DecendingOrder.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class DecendingOrder
+{
+	public static void main(String[] args) 
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter no of Decending Order array");
+	int a[]={4,-1,0,11,3};
+	for(int i=0;i<=(a.length)-1;i++)
+		{
+	for(int j=0;j<(a.length)-1;j++)
+		{
+		if(a[j]<a[j+1])
+			{
+			int c=a[j];
+			a[j]=a[j+1];
+			a[j+1]=c;
+			}
+		}
+		}
+	 System.out.print("a[]={");
+		 for (int i:a)
+		 {
+		 System.out.print(i+",");
+		 }
+		 System.out.println("}");
+	
+}
+}
+	
+
