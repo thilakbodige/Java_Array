a92ab7e4861be0f68155aec74869ed161546d343
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-02-18 14:14:17 +0530

    Add files via upload

---
+++ FirstShift_LeftShift.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class FirstShift_SecondHalf
+{
+	public static void main(String[] args) 
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter no of shift array");
+	int a[]={1,2,3,4,5,6};
+	int half_last=a[(a.length)-1];
+		for(int i=(a.length)-1;i>(a.length/2);i--)
+		{
+		a[i]=a[i-1];
+		}
+		a[(a.length)/2]=half_last;
+	int halflast=a[(a.length/2)-1];
+	 System.out.print("a[]={");
+		 for (int i:a)
+		 {
+		 System.out.print(i+",");
+		 }
+		 System.out.println("}");
+	}
+}
+	
